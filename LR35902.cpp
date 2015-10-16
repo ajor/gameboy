01c26d42ed802e2da3394dcc157cfcc423b2fac5
Alastair Robertson <alastair@ajor.co.uk>
2015-10-17 00:52:29 +0100

    Implement some loads

--- LR35902.cpp
+++ LR35902.cpp
@@ -92,224 +92,280 @@ void LR35902::LD_hl_n()
 
 void LR35902::LD_a_a()
 {
+  reg.a = reg.a;
 }
 
 void LR35902::LD_a_b()
 {
+  reg.a = reg.b;
 }
 
 void LR35902::LD_a_c()
 {
+  reg.a = reg.c;
 }
 
 void LR35902::LD_a_d()
 {
+  reg.a = reg.d;
 }
 
 void LR35902::LD_a_e()
 {
+  reg.a = reg.e;
 }
 
 void LR35902::LD_a_h()
 {
+  reg.a = reg.h;
 }
 
 void LR35902::LD_a_l()
 {
+  reg.a = reg.l;
 }
 
 void LR35902::LD_a_hl()
 {
+  reg.a = reg.hl;
 }
 
 void LR35902::LD_b_b()
 {
+  reg.b = reg.b;
 }
 
 void LR35902::LD_b_c()
 {
+  reg.b = reg.c;
 }
 
 void LR35902::LD_b_d()
 {
+  reg.b = reg.d;
 }
 
 void LR35902::LD_b_e()
 {
+  reg.b = reg.e;
 }
 
 void LR35902::LD_b_h()
 {
+  reg.b = reg.h;
 }
 
 void LR35902::LD_b_l()
 {
+  reg.b = reg.l;
 }
 
 void LR35902::LD_b_hl()
 {
+  reg.b = reg.hl;
 }
 
 void LR35902::LD_c_b()
 {
+  reg.c = reg.b;
 }
 
 void LR35902::LD_c_c()
 {
+  reg.c = reg.c;
 }
 
 void LR35902::LD_c_d()
 {
+  reg.c = reg.d;
 }
 
 void LR35902::LD_c_e()
 {
+  reg.c = reg.e;
 }
 
 void LR35902::LD_c_h()
 {
+  reg.c = reg.h;
 }
 
 void LR35902::LD_c_l()
 {
+  reg.c = reg.l;
 }
 
 void LR35902::LD_c_hl()
 {
+  reg.c = reg.hl;
 }
 
 void LR35902::LD_d_b()
 {
+  reg.d = reg.b;
 }
 
 void LR35902::LD_d_c()
 {
+  reg.d = reg.c;
 }
 
 void LR35902::LD_d_d()
 {
+  reg.d = reg.d;
 }
 
 void LR35902::LD_d_e()
 {
+  reg.d = reg.e;
 }
 
 void LR35902::LD_d_h()
 {
+  reg.d = reg.h;
 }
 
 void LR35902::LD_d_l()
 {
+  reg.d = reg.l;
 }
 
 void LR35902::LD_d_hl()
 {
+  reg.d = reg.hl;
 }
 
 void LR35902::LD_e_b()
 {
+  reg.e = reg.b;
 }
 
 void LR35902::LD_e_c()
 {
+  reg.e = reg.c;
 }
 
 void LR35902::LD_e_d()
 {
+  reg.e = reg.d;
 }
 
 void LR35902::LD_e_e()
 {
+  reg.e = reg.e;
 }
 
 void LR35902::LD_e_h()
 {
+  reg.e = reg.h;
 }
 
 void LR35902::LD_e_l()
 {
+  reg.e = reg.l;
 }
 
 void LR35902::LD_e_hl()
 {
+  reg.e = reg.hl;
 }
 
 void LR35902::LD_h_b()
 {
+  reg.h = reg.b;
 }
 
 void LR35902::LD_h_c()
 {
+  reg.h = reg.c;
 }
 
 void LR35902::LD_h_d()
 {
+  reg.h = reg.d;
 }
 
 void LR35902::LD_h_e()
 {
+  reg.h = reg.e;
 }
 
 void LR35902::LD_h_h()
 {
+  reg.h = reg.h;
 }
 
 void LR35902::LD_h_l()
 {
+  reg.h = reg.l;
 }
 
 void LR35902::LD_h_hl()
 {
+  reg.h = reg.hl;
 }
 
 void LR35902::LD_l_b()
 {
+  reg.l = reg.b;
 }
 
 void LR35902::LD_l_c()
 {
+  reg.l = reg.c;
 }
 
 void LR35902::LD_l_d()
 {
+  reg.l = reg.d;
 }
 
 void LR35902::LD_l_e()
 {
+  reg.l = reg.e;
 }
 
 void LR35902::LD_l_h()
 {
+  reg.l = reg.h;
 }
 
 void LR35902::LD_l_l()
 {
+  reg.l = reg.l;
 }
 
 void LR35902::LD_l_hl()
 {
+  reg.l = reg.hl;
 }
 
 void LR35902::LD_hl_b()
 {
+  reg.hl = reg.b;
 }
 
 void LR35902::LD_hl_c()
 {
+  reg.hl = reg.c;
 }
 
 void LR35902::LD_hl_d()
 {
+  reg.hl = reg.d;
 }
 
 void LR35902::LD_hl_e()
 {
+  reg.hl = reg.e;
 }
 
 void LR35902::LD_hl_h()
 {
+  reg.hl = reg.h;
 }
 
 void LR35902::LD_hl_l()
 {
+  reg.hl = reg.l;
 }
