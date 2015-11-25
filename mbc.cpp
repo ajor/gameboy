ba5f00fe19a5f9ef0b9e48a03bdbab9e5605500b
Alastair Robertson <alastair@ajor.co.uk>
2015-11-25 22:19:22 +0000

    MBC1

--- mbc.cpp
+++ mbc.cpp
@@ -9,11 +9,64 @@ u8 MBC1::get8(uint address) const
   }
   else if (address >= 0x4000 && address < 0x8000)
   {
-    // Switchable ROM bank
-    return rom[active_rom_bank_addr + address - 0x4000];
+    // Switchable ROM bank (1 - 127)
+    return rom[active_rom_bank*0x4000 + address - 0x4000];
   }
+  else if (address >= 0xa000 && address < 0xc000)
+  {
+    // Switchable RAM bank (0 - 3)
+    if (ram_enabled)
+      return ram[active_ram_bank*0x2000 + address - 0xa000];
+    else
+      return 0;
+  }
+
+  // Should never get here
+  return 0;
 }
 
 void MBC1::set8(uint address, u8 value)
 {
+  if (address < 0x2000)
+  {
+    // RAM enable
+    if ((value & 0xa) == 0xa)
+      ram_enabled = true;
+    else
+      ram_enabled = false;
+  }
+  else if (address >= 0x2000 && address < 0x4000)
+  {
+    // Select ROM bank (lower 5 bits)
+    value &= 0x1f;
+    if (value == 0)
+      value = 1;
+    active_rom_bank = (active_rom_bank & 0x60) | value;
+  }
+  else if (address >= 0x4000 && address < 0x6000)
+  {
+    // Select RAM bank / select ROM bank (upper 2 bits)
+    value &= 3;
+    if (banking_mode == BankingMode::RAM)
+    {
+      active_ram_bank = value;
+    }
+    else
+    {
+      active_rom_bank = (value << 5) | (active_rom_bank & 0x1f);
+    }
+  }
+  else if (address >= 0x6000 && address < 0x8000)
+  {
+    // Select ROM/RAM mode
+    if (value)
+      banking_mode = BankingMode::RAM;
+    else
+      banking_mode = BankingMode::ROM;
+  }
+  else if (address >= 0xa000 && address < 0xc000)
+  {
+    // Switchable RAM bank (0 - 3)
+    ram[active_ram_bank*0x2000 + address - 0xa000] = value;
+  }
 }
--- mbc.h
+++ mbc.h
@@ -4,25 +4,34 @@
 
 class MemoryBankController
 {
-protected:
-  u8 *rom;
-  u8 *ram;
-  uint active_rom_bank_addr = 0x4000; // default to bank 1
-
 public:
   MemoryBankController() = delete;
-  explicit MemoryBankController(u8 *rom, u8 *ram) : rom(rom), ram(ram) {}
+  MemoryBankController(u8 *rom, u8 *ram) : rom(rom), ram(ram) {}
   virtual ~MemoryBankController() {}
 
   virtual u8 get8(uint address) const = 0;
   virtual void set8(uint address, u8 value) = 0;
+
+  enum class BankingMode
+  {
+    ROM,
+    RAM
+  };
+
+protected:
+  u8 *rom;
+  u8 *ram;
+  uint active_rom_bank = 1;
+  uint active_ram_bank = 0;
+  bool ram_enabled = false;
+  BankingMode banking_mode = BankingMode::ROM;
 };
 
 class MBC1 final : public MemoryBankController
 {
 public:
   //using MemoryBankController::MemoryBankController;
-  explicit MBC1(u8 *rom, u8 *ram) : MemoryBankController(rom, ram) {}
+  MBC1(u8 *rom, u8 *ram) : MemoryBankController(rom, ram) {}
 
   u8 get8(uint address) const override;
   void set8(uint address, u8 value) override;
