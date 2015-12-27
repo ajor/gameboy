db291b4bdc80da6bd33d9b37c61ce40aa19200ea
Alastair Robertson <alastair@ajor.co.uk>
2015-12-27 19:01:13 +0000

    Display: Only update if display is enabled

--- display.cpp
+++ display.cpp
@@ -4,30 +4,35 @@
 
 void Display::update(uint cycles)
 {
-  scanline_counter -= cycles;
+  u8 LCDC = memory.get8(Memory::IO::LCDC);
 
-  if (scanline_counter <= 0)
+  if (LCDC & (1<<7)) // LCD enabled?
   {
-    scanline_counter += cycles_per_scanline;
+    scanline_counter -= cycles;
 
-    u8 scanline = memory.get8(Memory::IO::LY) + 1;
-    if (scanline > 153)
+    if (scanline_counter <= 0)
     {
-      scanline = 0;
-    }
-    memory.direct_io_write8(Memory::IO::LY, scanline);
+      scanline_counter += cycles_per_scanline;
 
-    if (scanline < 144)
-    {
-      draw_scanline();
+      u8 scanline = memory.get8(Memory::IO::LY) + 1;
+      if (scanline > 153)
+      {
+        scanline = 0;
+      }
+      memory.direct_io_write8(Memory::IO::LY, scanline);
+
+      if (scanline < 144)
+      {
+        draw_scanline();
+      }
+      else if (scanline == 144)
+      {
+        cpu.raise_interrupt(LR35902::Interrupt::VBLANK);
+      }
     }
-    else if (scanline == 144)
-    {
-      cpu.raise_interrupt(LR35902::Interrupt::VBLANK);
-    }
+
+    update_status(); // put at beginning?
   }
-
-  update_status(); // put at beginning?
 }
 
 void Display::draw_scanline()
